c9656a2197a5208cfa63906d481a1c3911a6c287
ClMacher <clmacher@gmail.com>
2022-05-07 04:51:30 -0400

    creacion-tablero

---
+++ src/app/tablero/tablero.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tablero',
+  templateUrl: './tablero.component.html',
+  styleUrls: ['./tablero.component.css']
+})
+export class TableroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
